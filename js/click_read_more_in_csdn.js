003072f132e981d79c4665d57a389868b903467b
iinux <iinux@qq.com>
2019-06-30 18:45:46 +0800

    csdn update

--- js/click_read_more_in_csdn.js
+++ js/click_read_more_in_csdn.js
@@ -1,2 +1,7 @@
 //$("#btn-readmore").click();
-document.getElementById("btn-readmore").click();
+var dom = document.getElementById("btn-readmore");
+if (dom) {
+    dom.click();
+} else {
+    document.getElementsByClassName("btn-readmore")[0].click();
+}
