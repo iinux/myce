fd9d38cc1d57927402937084b154241f2c9eb4d6
iinux <iinux@qq.com>
2019-07-25 21:19:35 +0800

    readmore click in zhihu

--- js/click_read_more_in_csdn.js
+++ js/click_read_more_in_csdn.js
@@ -2,6 +2,14 @@
 var dom = document.getElementById("btn-readmore");
 if (dom) {
     dom.click();
-} else {
-    document.getElementsByClassName("btn-readmore")[0].click();
+}
+
+dom = document.getElementsByClassName("btn-readmore")[0];
+if (dom) {
+    dom.click();
+}
+
+dom = document.getElementsByClassName("QuestionRichText--collapsed")[0];
+if (dom) {
+    dom.click()
 }
